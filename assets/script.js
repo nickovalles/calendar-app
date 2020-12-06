44f04c1ae871908180c8f8dda6ebff39583c86e3
Nilda <nick.ovalles@gmail.com>
2020-12-06 13:41:26 -0700

    <timeSlot created in JS file>

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,19 @@
 // Time, Date, etc START
 
+let timeSlot = {
+    "7AM": "",
+    "8AM": "",
+    "9AM": "",
+    "10AM": "",
+    "11AM": "",
+    "12PM": "",
+    "1PM": "",
+    "2PM": "",
+    "3PM": "",
+    "4PM": "",
+};
+
+
 // Time, Date, etc END
 
 
